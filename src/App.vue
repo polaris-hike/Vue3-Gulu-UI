8dbb113e2b685296660ff638dbfc0664c149d1b4
wuxuweilalala <2509165479@qq.com>
2020-09-29 18:09:28 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    我的第一个组件
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
