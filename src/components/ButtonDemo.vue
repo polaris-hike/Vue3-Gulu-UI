b7c02297c7349131a1b337332b1fa3f8d2300130
wuxuweilalala <2509165479@qq.com>
2020-09-30 11:49:10 +0800

    css minimum impact

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,17 +1,29 @@
 <template>
     <div>Button 示例</div>
     <h1>示例1</h1>
-    <div><Button>你好</Button></div>
+    <div>
+        <Button >你好</Button>
+        <Button theme="button">你好</Button>
+        <Button theme="link">你好</Button>
+        <Button theme="text">你好</Button>
+    </div>
 </template>
 
 <script lang="ts">
-    import Button from "../libs/Button.vue";
+    import Button from '../libs/Button.vue';
+
     export default {
-        name: "ButtonDemo",
-        components:{
+        name: 'ButtonDemo',
+        components: {
             Button
+        },
+        setup(){
+            const onClick = ()=>{
+                console.log('hello world');
+            }
+            return {onClick}
         }
-    }
+    };
 </script>
 
 <style scoped>
--- src/libs/Switch.vue
+++ src/libs/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :class="{checked:value}" @click="toggle"><span></span></button>
+    <button  class="gulu-switch" :class="{'gulu-checked':value}" @click="toggle"><span></span></button>
 </template>
 
 <script lang="ts">
@@ -17,10 +17,10 @@
     };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
     $h: 22px;
     $h2: $h - 4px;
-    button {
+    .gulu-switch {
         height: $h;
         width: $h*2;
         border: none;
@@ -44,7 +44,7 @@
         &:active {
             > span {width: $h2 + 4px;}
         }
-        &.checked {
+        &.gulu-checked {
             background: #1890ff;
 
             > span {
