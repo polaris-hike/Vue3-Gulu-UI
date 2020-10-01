6d488a3a746a121b14b3e40f4bc43df2830e8d83
wuxuweilalala <2509165479@qq.com>
2020-10-01 12:02:51 +0800

    create Tabs and Tab

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,13 +1,19 @@
 <template>
-    Tabs
+    <div>Tabs 示例</div>
+    <h1>示例1</h1>
+    <Tabs>
+        <Tab :title="导航1">内容一</Tab>
+        <Tab :title="导航2">内容二</Tab>
+    </Tabs>
 </template>
 
 <script>
     import Tabs from "../libs/Tabs.vue";
+    import Tab from "../libs/Tab.vue";
     export default {
         name: "TabsDemo",
         components:{
-            Tabs
+            Tabs,Tab
         }
     }
 </script>
---
+++ src/libs/Tab.vue
@@ -0,0 +1,13 @@
+<template>
+    Tab 组件
+</template>
+
+<script>
+    export default {
+        name: "Tab"
+    }
+</script>
+
+<style scoped>
+
+</style>
