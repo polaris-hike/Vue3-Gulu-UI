f11d8d19d5ef5dfb63b9663e7003313cc997b646
wuxuweilalala <2509165479@qq.com>
2020-10-01 10:57:56 +0800

    dialog css and visible

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,13 +1,22 @@
 <template>
-    Dialog
+    <div>Dialog 示例</div>
+    <h1>示例1</h1>
+    <Button @click="visible = true">toggle</Button>
+    <Dialog :visible="visible"></Dialog>
 </template>
 
-<script>
+<script lang="ts">
     import Dialog from "../libs/Dialog.vue";
+    import Button from '../libs/Button.vue';
+    import  {ref} from 'vue';
     export default {
         name: "DialogDemo",
         components:{
-            Dialog
+            Dialog,Button
+        },
+        setup(){
+            const visible = ref(false);
+            return {visible}
         }
     }
 </script>
