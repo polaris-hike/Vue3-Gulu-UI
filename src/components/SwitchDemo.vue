09a294f5442293dd779b191f68bbb8c20a9b6cca
wuxuweilalala <2509165479@qq.com>
2020-10-09 22:07:32 +0800

    optimize coded

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,52 +1,24 @@
 <template>
     <div>
         <h1>Switch 组件示例 </h1>
-        <div class="demo">
-            <h2>常规用法</h2>
-            <div class="demo-component">
-                <component :is="Switch1Demo"></component>
-            </div>
-            <div class="demo-actions">
-                <Button>查看代码</Button>
-            </div>
-            <div class="demo-code">
-                <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode,Prism.languages.html,'html') "/>
-            </div>
-        </div>
-        <div class="demo">
-            <h2>支持 disabled </h2>
-            <div class="demo-component">
-                <component :is="Switch2Demo"></component>
-            </div>
-            <div class="demo-actions">
-                <Button>查看代码</Button>
-            </div>
-            <div class="demo-code">
-              <pre class="language-html" v-html="Prism.highlight(Switch2Demo.__sourceCode,Prism.languages.html,'html') "/>
-            </div>
-        </div>
+        <Demo :component="Switch1Demo"></Demo>
+        <Demo :component="Switch2Demo"></Demo>
     </div>
 </template>
 
 <script lang="ts">
-    import Switch from '../libs/Switch.vue';
-    import Button from '../libs/Button.vue';
+    import Demo from './Demo.vue';
     import Switch1Demo from './Switch1.demo.vue';
     import Switch2Demo from './Switch2.demo.vue';
-    import 'prismjs';
-    import 'prismjs/themes/prism.css';
-    const Prism = (window as any).Prism
-    console.log(Prism);
-    import {ref} from 'vue';
 
     export default {
         name: 'SwitchDemo',
         components: {
-            Switch, Button
+            Demo
         },
         setup() {
-            const isChecked = ref(false);
-            return {isChecked, Switch1Demo, Switch2Demo,Prism};
+
+            return {Switch1Demo, Switch2Demo};
         }
     };
 </script>
