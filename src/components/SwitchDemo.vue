0e26da342913d362db69db3c6a4c81d70f3e23c8
wuxuweilalala <2509165479@qq.com>
2020-10-09 21:52:41 +0800

    hightlight code

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,77 +1,85 @@
-
 <template>
-  <div>
-    <h1>Switch 组件示例 </h1>
-    <div class="demo">
-      <h2>常规用法</h2>
-      <div class="demo-component">
-        <Switch1Demo />
-      </div>
-      <div class="demo-actions">
-        <Button>查看代码</Button>
-      </div>
-      <div class="demo-code">
-        <pre>{{Switch1Demo.__sourceCode}}</pre>
-      </div>
+    <div>
+        <h1>Switch 组件示例 </h1>
+        <div class="demo">
+            <h2>常规用法</h2>
+            <div class="demo-component">
+                <component :is="Switch1Demo"></component>
+            </div>
+            <div class="demo-actions">
+                <Button>查看代码</Button>
+            </div>
+            <div class="demo-code">
+                <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode,Prism.languages.html,'html') "/>
+            </div>
+        </div>
+        <div class="demo">
+            <h2>支持 disabled </h2>
+            <div class="demo-component">
+                <component :is="Switch2Demo"></component>
+            </div>
+            <div class="demo-actions">
+                <Button>查看代码</Button>
+            </div>
+            <div class="demo-code">
+              <pre class="language-html" v-html="Prism.highlight(Switch2Demo.__sourceCode,Prism.languages.html,'html') "/>
+            </div>
+        </div>
     </div>
-    <div class="demo">
-      <h2>支持 disabled </h2>
-      <div class="demo-component">
-        <Switch2Demo />
-      </div>
-      <div class="demo-actions">
-        <Button>查看代码</Button>
-      </div>
-      <div class="demo-code">
-        <pre>{{Switch2Demo.__sourceCode}}</pre>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script lang="ts">
     import Switch from '../libs/Switch.vue';
-    import Button from '../libs/Button.vue'
-    import Switch1Demo from './Switch1.demo.vue'
-    import Switch2Demo from './Switch2.demo.vue'
-    import {ref} from 'vue'
+    import Button from '../libs/Button.vue';
+    import Switch1Demo from './Switch1.demo.vue';
+    import Switch2Demo from './Switch2.demo.vue';
+    import 'prismjs';
+    import 'prismjs/themes/prism.css';
+    const Prism = (window as any).Prism
+    console.log(Prism);
+    import {ref} from 'vue';
 
     export default {
         name: 'SwitchDemo',
         components: {
-            Switch,Button,Switch1Demo,Switch2Demo
+            Switch, Button
         },
-      setup(){
-          const isChecked = ref(false);
-          return {isChecked,Switch1Demo,Switch2Demo}
-      }
+        setup() {
+            const isChecked = ref(false);
+            return {isChecked, Switch1Demo, Switch2Demo,Prism};
+        }
     };
 </script>
 <style lang="scss" scoped>
-  $border-color: #d9d9d9;
-  .demo {
-    border: 1px solid $border-color;
-    margin: 16px 0 32px;
-    >h2 {
-      font-size: 20px;
-      padding: 8px 16px;
-      border-bottom: 1px solid $border-color;
+    $border-color: #d9d9d9;
+    .demo {
+        border: 1px solid $border-color;
+        margin: 16px 0 32px;
+
+        > h2 {
+            font-size: 20px;
+            padding: 8px 16px;
+            border-bottom: 1px solid $border-color;
+        }
+
+        &-component {
+            padding: 16px;
+        }
+
+        &-actions {
+            padding: 8px 16px;
+            border-top: 1px dashed $border-color;
+        }
+
+        &-code {
+            padding: 8px 16px;
+            border-top: 1px dashed $border-color;
+
+            > pre {
+                line-height: 1.1;
+                font-family: Consolas, 'Courier New', Courier, monospace;
+                margin: 0;
+            }
+        }
     }
-    &-component {
-      padding: 16px;
-    }
-    &-actions {
-      padding: 8px 16px;
-      border-top: 1px dashed $border-color;
-    }
-    &-code {
-      padding: 8px 16px;
-      border-top: 1px dashed $border-color;
-      >pre {
-        line-height: 1.1;
-        font-family: Consolas, 'Courier New', Courier, monospace;
-        margin: 0;
-      }
-    }
-  }
 </style>
